4ecfebba430e3c25b5e71e76e5866733bce186a9
Igor <zaplatynskyy@gmail.com>
2021-11-15 15:05:07 +0100

    jsnack 2

--- js/script.js
+++ js/script.js
@@ -13,25 +13,36 @@
 
 // console.log('La somma dei numeri inseriti è', sum);
 
-// versione ciclo while
-let sum = 0;
-let i = 0;
+// // versione ciclo while
+// let sum = 0;
+// let i = 0;
 
-while(i < 5) {
-    const num = parseInt(prompt('Inserisci un numero'));
-    sum += num; 
+// while(i < 5) {
+//     const num = parseInt(prompt('Inserisci un numero'));
+//     sum += num; 
 
-    i++;
-}
+//     i++;
+// }
 
-console.log('La somma dei numeri inseriti è', sum);
-
+// console.log('La somma dei numeri inseriti è', sum);
 
 
 // JSnack 2
 // Dare la possibilità di inserire due parole.
 // Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
 // Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+const parolaUno = prompt('Inserisci la prima parola');
+const parolaDue = prompt('Inserisci la seconda parola');
+
+if(parolaUno.length < parolaDue.length) {
+    console.log('La parola più lunga è', parolaDue);
+} else if(parolaUno.length > parolaDue.length) {
+    console.log('La parola più lunga è', parolaUno);
+} else {
+    console.log('Le parole inserite (', parolaUno,'e', parolaDue, ') hanno la stessa lunghezza');
+}
+
 
 
 // JSnack 3
